354eca61347461967ede68e0563c8598f8e83e8d
Umeshkumar24 <dasumesh957@gmail.com>
2024-11-27 23:40:26 +0530

    BUG
    BUG FIXES

--- noter_1_0/src/main/java/com/noter/noter_1_0/Noter10Application.java
+++ noter_1_0/src/main/java/com/noter/noter_1_0/Noter10Application.java
@@ -2,11 +2,19 @@ package com.noter.noter_1_0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Noter10Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Noter10Application.class, args);
-	}
 
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String username = "UMESH";
+		String password = "1234";
+		if (DatabaseConnectionChecker.isDatabaseConnected(url, username, password)) {
+			SpringApplication.run(Noter10Application.class, args);
+		} else {
+			System.err.println("Failed to connect to the database. Application will not start.");
+		}
+	}
 }
